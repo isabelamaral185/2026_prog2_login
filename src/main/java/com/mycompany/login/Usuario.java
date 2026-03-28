e68b69254e414b7bed84495145ef1bb7e8b6ee4f
Aluno <Aluno@10.60.2.247>
2026-03-27 22:27:11 -0300

    iniciando projeto

---
+++ src/main/java/com/mycompany/login/Usuario.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.login;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Usuario {
+    private String email;
+    private String senha;
+    private int idade;
+
+    public Usuario() {
+    }
+
+    public Usuario(String email, String senha, int idade) {
+        this.email = email;
+        this.senha = senha;
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "email=" + email + ", senha=" + senha + ", idade=" + idade + '}';
+    } // Vai mostrar todos os dados daquele objeto.
+}
